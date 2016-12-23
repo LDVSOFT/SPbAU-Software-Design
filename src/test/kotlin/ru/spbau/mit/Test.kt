62913c2d96d300f7244c25816fc36f171b53156f
LDVSOFT <lapshin.dv@mail.ru>
2016-12-24 02:02:15 +0300

    Added Cd, Pwd and Ls tests

--- src/test/kotlin/ru/spbau/mit/Test.kt
+++ src/test/kotlin/ru/spbau/mit/Test.kt
@@ -3,6 +3,9 @@ package ru.spbau.mit
 import org.junit.Before
 import org.junit.Test
 import ru.spbau.mit.commands.*
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
 import kotlin.test.assertEquals
 
 /**
@@ -18,6 +21,9 @@ class Test {
         shell.registerCommand("wc", Wc())
         shell.registerCommand("cat", Cat())
         shell.registerCommand("grep", Grep())
+        shell.registerCommand("ls", Ls())
+        shell.registerCommand("cd", Cd())
+        shell.registerCommand("pwd", Pwd())
     }
 
     @Test
@@ -34,5 +40,21 @@ class Test {
     @Test
     fun testGrep() {
         assertEquals("1 1 34", shell.execute("grep modelVersion pom.xml | wc"))
+    }
+
+    @Test
+    fun testCdPwd() {
+        val cwd = Paths.get(".").normalize().toAbsolutePath();
+        assertEquals(cwd.toString(), shell.execute("pwd"))
+        assertEquals("", shell.execute("cd src"))
+        assertEquals(cwd.resolve("src").toString(), shell.execute("pwd"))
+    }
+
+    @Test
+    fun testLs() {
+        val path = Paths.get("src", "main", "kotlin", "ru", "spbau", "mit")
+        val ls = ArrayList<String>()
+        Files.newDirectoryStream(path).forEach { ls.add(it.fileName.toString()) }
+        assertEquals(ls.joinToString(separator = "\n", postfix = "\n"), shell.execute("ls " + path))
     }
 }
